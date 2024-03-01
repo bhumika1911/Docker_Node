96f4943eb8dc404018363858bab254bff96106e4
bhumika1911 <98683980+bhumika1911@users.noreply.github.com>
2024-03-01 17:57:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const studentRoutes=require("./src/student/routes");
 const app= express();   
-const port= 7000;
+const port= 3000;
 const path=require('path');
 
 app.use(express.json());    
@@ -14,6 +14,10 @@ app.get("/",async(req,res)=>{
 // app.get("/", (req,res)=>{
 //     res.send("hello world");
 // });
+app.put("/api/v1/students/:id", (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body; 
+});
 
 app.use("/api/v1/students", studentRoutes);
 app.listen(port,() => console.log(`app listening on port ${port}`));
