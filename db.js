4665f9952ff984ee34e82434914e9aedbaf69502
bhumika1911 <98683980+bhumika1911@users.noreply.github.com>
2024-03-06 23:06:37 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Pool=require('pg').Pool;
+// const { Pool } = require('pg'); 
 const pool=new Pool({
     user:"postgres",
-    host:"localhost",
+    host:"host.docker.internal",
     database:"students",
     password:"bhumigres",
     port:5432,
